2257c2b699a8c26d550af3a6dc4b153936eac795
Edwin Obando <webeao@gmail.com>
2020-04-05 13:01:14 -0500

    Primer commit

---
+++ server/server.js
@@ -0,0 +1,51 @@
+require('./config/config');
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+//Convención para obtener
+app.get('/usuario', function (req, res) {
+    res.json('Hello word')
+});
+// POST convencion para crear 
+app.post('/usuario', function (req, res) {
+
+ 
+
+    let body = req.body;
+
+    if ( body.nombre === undefined){
+            res.status(400).json({
+                ok: false,
+                mensaje: 'El nombre es necesario'
+            })
+    }else{
+    res.json({
+        persona: body
+    });
+}
+});
+// PUT convención para actualizar datos
+app.put('/usuario/:id', function (req, res) {
+let id = req.params.id;
+    res.json({
+        id
+    });
+});
+// DELETE convención para eleiminarr 
+app.delete('/usuario', function (req, res) {
+    res.json('Hello word')
+});
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log('Escuchando el puerto: ', process.env.PORT)
+});
